ac79c10dac49e200063e5bc75620e8b1bfe5d621
HamdanMahmud <212084901+HamdanMahmud@users.noreply.github.com>
2026-09-22 14:41:49 +0600

    Add files via upload

---
+++ Day 2/YourWork/cpday2-2.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<string,int>student;
+    student.first = "Hamdan";
+    student.second = 31;
+    cout<<"Name: "<<student.first<< ",Roll: "<<student.second << "\n";
+    return 0;
+}
